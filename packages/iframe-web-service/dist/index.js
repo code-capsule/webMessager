b2c71b93a871d84d0079f9ddcee8b6a72e024ff0
LinDaiDai <244508795@qq.com>
2022-10-11 16:53:59 +0800

    [modify]提交dist文件

--- packages/iframe-web-service/dist/index.js
+++ packages/iframe-web-service/dist/index.js
@@ -1,53 +1,49 @@
-'use strict'
+"use strict";
 
-var _interopRequireDefault = require('@babel/runtime/helpers/interopRequireDefault')
+var _interopRequireDefault = require("@babel/runtime/helpers/interopRequireDefault");
 
-Object.defineProperty(exports, '__esModule', {
-  value: true,
-})
-exports.default = void 0
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+exports.default = void 0;
 
-var _createClass2 = _interopRequireDefault(
-  require('@babel/runtime/helpers/createClass'),
-)
+var _createClass2 = _interopRequireDefault(require("@babel/runtime/helpers/createClass"));
 
-var _classCallCheck2 = _interopRequireDefault(
-  require('@babel/runtime/helpers/classCallCheck'),
-)
+var _classCallCheck2 = _interopRequireDefault(require("@babel/runtime/helpers/classCallCheck"));
 
-var _webService = _interopRequireDefault(require('@codecapsule/web-service'))
+var _webService = _interopRequireDefault(require("@codecapsule/web-service"));
 
-var _webMessager = require('@codecapsule/web-messager')
+var _webMessager = require("@codecapsule/web-messager");
 
-var IFrameWebService = /*#__PURE__*/ (0, _createClass2.default)(
-  function IFrameWebService(props) {
-    ;(0, _classCallCheck2.default)(this, IFrameWebService)
-    var messagerType = props.messagerType,
-      iframe = props.iframe
+var IFrameWebService = /*#__PURE__*/(0, _createClass2.default)(function IFrameWebService(props) {
+  (0, _classCallCheck2.default)(this, IFrameWebService);
+  var messagerType = props.messagerType,
+      iframe = props.iframe;
 
-    switch (messagerType) {
-      case 'parent':
-        _webMessager.parentIframeMessager.setIframe(iframe)
+  switch (messagerType) {
+    case 'parent':
+      var parentIframeMessager = new _webMessager.ParentIframeMessager({
+        iframe: iframe
+      });
+      return new _webService.default({
+        messager: parentIframeMessager
+      });
 
-        return new _webService.default({
-          messager: _webMessager.parentIframeMessager,
-        })
-
-      case 'child':
-        return new _webService.default({
-          messager: _webMessager.childIframeMessager,
-        })
-    }
-  },
-)
-var webService
+    case 'child':
+      var childIframeMessager = new _webMessager.ChildIframeMessager();
+      return new _webService.default({
+        messager: childIframeMessager
+      });
+  }
+});
+var webService;
 
 var _default = function _default(props) {
   if (!webService) {
-    return new IFrameWebService(props)
+    return new IFrameWebService(props);
   }
 
-  return webService
-}
+  return webService;
+};
 
-exports.default = _default
+exports.default = _default;
